c8ffc6f92b88c71d8c13bb4cc11116e793d3a335
jreicheneker <jreicheneker@server>
2012-08-01 15:20:55 -0600

    Section 3.5 Exercise 1 - Add a Contact page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ RailstutorialSampleApp::Application.routes.draw do
 
   get "static_pages/about"
 
+  get "static_pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
